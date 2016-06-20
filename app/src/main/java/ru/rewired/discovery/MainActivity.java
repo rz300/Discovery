3c383dfe08161c06f4bf049ce12b35d5b303c7bd
KalmykovIV <rz300@ya.ru>
2016-06-20 10:33:28 +0300

    base project structure

---
+++ app/src/main/java/ru/rewired/discovery/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.rewired.discovery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by KalmykovIV on 20.06.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
